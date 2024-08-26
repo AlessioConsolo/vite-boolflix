079f29f3566726e2617f9cbe2ffed925414479b8
AlessioConsolo <alessioconsolo3@gmail.com>
2024-08-26 17:45:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import filmSection from "./components/FilmSection.vue";
+import TVSection from "./components/TVSection.vue";
+</script>
+
+<template>
+  <TVSection />
+  <filmSection />
+  <title>Ciao</title>
+</template>
+
+<style scoped></style>
