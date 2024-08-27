65b9bfd8a4a5d4b12a1bbfc7b3d5fc9fb0037270
AlessioConsolo <alessioconsolo3@gmail.com>
2024-08-27 16:27:16 +0200

    [UPD] Renamed FilmSection.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-import filmSection from "./components/FilmSection.vue";
+import FilmSection from "./components/FilmSection.vue";
 import TVSection from "./components/TVSection.vue";
 import AppMain from "./components/AppMain.vue";
 </script>
 
 <template>
   <TVSection />
-  <filmSection />
+  <FilmSection />
   <AppMain />
 </template>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,41 @@
-<template lang="html">
-  <form>
-    <input type="search" name="searchbar" id="searchbar" />
-    <button type="button">Cerca</button>
-  </form>
+<template>
+  <div>
+    <input type="text" v-model="searchQuery" />
+    <button @click="searchMovies">Cerca</button>
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        {{ movie.title }} <br />
+        {{ movie.original_title }} <br />
+        {{ movie.original_language }} <br />
+        {{ movie.vote_average }}
+      </li>
+    </ul>
+  </div>
 </template>
+
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      searchQuery: "",
+      movies: [],
+    };
+  },
+  methods: {
+    searchMovies() {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=cb4e8200a9d894d0b396d2d7fe14c7ed&query=${this.searchQuery}`
+        )
+        .then((response) => {
+          this.movies = response.data.results;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
 </script>
-<style lang=""></style>
