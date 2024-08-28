29fc05f7ca4bf8d029fe49c5b5b42f071cf01e0a
AlessioConsolo <alessioconsolo3@gmail.com>
2024-08-28 05:32:03 +0200

    [UPD] Added in both film and tvsection mouse hover event and styled cards to display information based on the film/tvshow

--- src/components/TVSection.vue
+++ src/components/TVSection.vue
@@ -3,27 +3,40 @@
     <h2>Serie TV</h2>
     <ul>
       <li v-for="tvShow in tvShows" :key="tvShow.id">
-        {{ tvShow.name }} <br />
-        {{ tvShow.original_name }} <br />
-        <span v-html="getFlagIcon(tvShow.original_language)"></span> <br />
-        <div class="rating">
-          <i
-            v-for="n in getRating(tvShow.vote_average)"
-            :key="n"
-            class="fa-solid fa-star"
-          ></i>
-          <i
-            v-for="n in 5 - getRating(tvShow.vote_average)"
-            :key="n"
-            class="fa-regular fa-star"
-          ></i>
+        <div
+          class="card"
+          @mouseover="hoveredTvShowId = tvShow.id"
+          @mouseleave="hoveredTvShowId = null"
+        >
+          <img
+            :src="`https://image.tmdb.org/t/p/w342${tvShow.poster_path}`"
+            alt="Copertina della serie TV"
+            class="card-img-top"
+            :style="{ opacity: hoveredTvShowId === tvShow.id ? 0 : 1 }"
+          />
+          <div class="card-overlay" v-if="hoveredTvShowId === tvShow.id">
+            <div class="card-info">
+              {{ tvShow.name }} <br />
+              {{ tvShow.original_name }} <br />
+              <span v-html="getFlagIcon(tvShow.original_language)"></span>
+              <br />
+              <div class="rating">
+                <i
+                  v-for="n in getRating(tvShow.vote_average)"
+                  :key="n"
+                  class="fa-solid fa-star"
+                ></i>
+                <i
+                  v-for="n in 5 - getRating(tvShow.vote_average)"
+                  :key="n"
+                  class="fa-regular fa-star"
+                ></i>
+              </div>
+              <br />
+              {{ tvShow.overview }}
+            </div>
+          </div>
         </div>
-        <br />
-        <img
-          :src="`https://image.tmdb.org/t/p/w342${tvShow.poster_path}`"
-          alt="Copertina del film"
-        />
-        <br />
       </li>
     </ul>
   </section>
@@ -37,6 +50,7 @@ export default {
   data() {
     return {
       tvShows: [],
+      hoveredTvShowId: null,
     };
   },
   watch: {
@@ -72,3 +86,58 @@ export default {
   },
 };
 </script>
+
+<style>
+.card {
+  position: relative;
+  width: 342px;
+  height: 300px;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.card-img-top {
+  width: 342px;
+  height: 500px;
+  object-fit: cover;
+  transition: opacity 0.3s ease-in-out;
+}
+
+.card-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-info {
+  color: #fff;
+  font-size: 16px;
+  padding: 20px;
+  text-align: center;
+  overflow-y: auto;
+}
+
+.rating {
+  display: inline-block;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+li {
+  display: inline-block;
+  margin: 10px;
+}
+</style>
--- src/components/filmSection.vue
+++ src/components/filmSection.vue
@@ -3,26 +3,40 @@
     <h2>Film</h2>
     <ul>
       <li v-for="movie in movies" :key="movie.id">
-        {{ movie.title }} <br />
-        {{ movie.original_title }} <br />
-        <span v-html="getFlagIcon(movie.original_language)"></span> <br />
-        <div class="rating">
-          <i
-            v-for="n in getRating(movie.vote_average)"
-            :key="n"
-            class="fa-solid fa-star"
-          ></i>
-          <i
-            v-for="n in 5 - getRating(movie.vote_average)"
-            :key="n"
-            class="fa-regular fa-star"
-          ></i>
+        <div
+          class="card"
+          @mouseover="hoveredmovieId = movie.id"
+          @mouseleave="hoveredmovieId = null"
+        >
+          <img
+            :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`"
+            alt="Copertina del film"
+            class="card-img-top"
+            :style="{ opacity: hoveredmovieId === movie.id ? 0 : 1 }"
+          />
+          <div class="card-overlay" v-if="hoveredmovieId === movie.id">
+            <div class="card-info">
+              {{ movie.title }} <br />
+              {{ movie.original_title }} <br />
+              <span v-html="getFlagIcon(movie.original_language)"></span>
+              <br />
+              <div class="rating">
+                <i
+                  v-for="n in getRating(movie.vote_average)"
+                  :key="n"
+                  class="fa-solid fa-star"
+                ></i>
+                <i
+                  v-for="n in 5 - getRating(movie.vote_average)"
+                  :key="n"
+                  class="fa-regular fa-star"
+                ></i>
+              </div>
+              <br />
+              {{ movie.overview }}
+            </div>
+          </div>
         </div>
-        <br />
-        <img
-          :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`"
-          alt="Copertina del film"
-        />
       </li>
     </ul>
   </section>
@@ -36,6 +50,7 @@ export default {
   data() {
     return {
       movies: [],
+      hoveredmovieId: null,
     };
   },
   watch: {
@@ -71,3 +86,58 @@ export default {
   },
 };
 </script>
+
+<style>
+.card {
+  position: relative;
+  width: 342px;
+  height: 300px;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.card-img-top {
+  width: 342px;
+  height: 500px;
+  object-fit: cover;
+  transition: opacity 0.3s ease-in-out;
+}
+
+.card-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-info {
+  color: #fff;
+  font-size: 16px;
+  padding: 20px;
+  text-align: center;
+  overflow-y: auto;
+}
+
+.rating {
+  display: inline-block;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+li {
+  display: inline-block;
+  margin: 10px;
+}
+</style>
